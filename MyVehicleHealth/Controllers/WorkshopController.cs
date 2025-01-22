a825e0c330368428c7d32cec243026d30f1c867c
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2025-01-22 17:13:05 -0300

    CRUD de oficinas criado

---
+++ MyVehicleHealth/Controllers/WorkshopController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using MyVehicleHealth.Data;
+using MyVehicleHealth.Models;
+
+namespace MyVehicleHealth.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WorkshopController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public WorkshopController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAll() => Ok(_context.Workshops.ToList());
+    
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var workshop = _context.Workshops.Find(id);
+        return workshop is null ? NotFound() : Ok(workshop);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Workshop workshop)
+    {
+        _context.Workshops.Add(workshop);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetById), new { id = workshop.Id }, workshop);
+    }
+    
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Workshop updatedWorkshop)
+    {
+        var workshop = _context.Workshops.Find(id);
+        if (workshop is null) return NotFound();
+        
+        workshop.CompanyName = updatedWorkshop.CompanyName;
+        workshop.MechanicName = updatedWorkshop.MechanicName;
+        workshop.Phone = updatedWorkshop.Phone;
+        _context.SaveChanges();
+        
+        return CreatedAtAction(nameof(GetById), new { id = workshop.Id }, workshop);
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var workshop = _context.Workshops.Find(id);
+        if (workshop is null) return NotFound();
+        _context.Workshops.Remove(workshop);
+        _context.SaveChanges();
+        return NoContent();
+    }
+    
+}
