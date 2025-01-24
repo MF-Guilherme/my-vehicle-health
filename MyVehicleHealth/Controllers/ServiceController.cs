65778519947a1fca3c40e61bd3824cc5b618edf1
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2025-01-24 16:30:38 -0300

    GetById, GetAllServicesForMaintenance and Create endpoints for Service

--- MyVehicleHealth/Controllers/ServiceController.cs
+++ MyVehicleHealth/Controllers/ServiceController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyVehicleHealth.Data;
 using MyVehicleHealth.Dtos;
+using MyVehicleHealth.Models;
 
 namespace MyVehicleHealth.Controllers;
 
@@ -19,6 +21,7 @@ public class ServiceController : ControllerBase
     public IActionResult GetAll()
     {
         var services = _context.Services
+            .Include(s => s.Maintenance)
             .Select(s => new ServiceReadDto
             {
                 Id = s.Id,
@@ -35,5 +38,73 @@ public class ServiceController : ControllerBase
             .ToList();
         return Ok(services);
 
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var service = _context.Services
+            .Where(s => s.Id == id)
+            .Include(s => s.Maintenance)
+            .Select(s => new ServiceReadDto
+            {
+                Id = s.Id,
+                MaintenanceId = s.MaintenanceId,
+                Description = s.Description,
+                PartCost = s.PartCost,
+                LaborCost = s.LaborCost,
+                MaintenanceDate = s.MaintenanceDate,
+                NextMaintenanceDate = s.NextMaintenanceDate,
+                CurrentMileage = s.CurrentMileage,
+                NextMaintenanceMileage = s.NextMaintenanceMileage,
+                PartBrand = s.PartBrand
+            })
+            .FirstOrDefault();
+        return Ok(service);
+
+    }
+    
+    [HttpGet("maintenance/{maintenanceId}")]
+    public IActionResult GetAllServicesForMaintenance(int maintenanceId)
+    {
+        var services = _context.Services
+            .Where(s => s.MaintenanceId == maintenanceId)
+            .Include(s => s.Maintenance)
+            .Select(s => new ServiceReadDto
+            {
+                Id = s.Id,
+                MaintenanceId = s.MaintenanceId,
+                Description = s.Description,
+                PartCost = s.PartCost,
+                LaborCost = s.LaborCost,
+                MaintenanceDate = s.MaintenanceDate,
+                NextMaintenanceDate = s.NextMaintenanceDate,
+                CurrentMileage = s.CurrentMileage,
+                NextMaintenanceMileage = s.NextMaintenanceMileage,
+                PartBrand = s.PartBrand
+            })
+            .ToList();
+        return Ok(services);
+
+    }
+    
+    [HttpPost]
+    public IActionResult Create(int maintenanceId, ServiceCreateDto dto)
+    {
+        var service = new Service
+        {
+            MaintenanceId = maintenanceId,
+            Description = dto.Description,
+            MaintenanceDate = dto.MaintenanceDate,
+            CurrentMileage = dto.CurrentMileage,
+            NextMaintenanceDate = dto.NextMaintenanceDate,
+            NextMaintenanceMileage = dto.NextMaintenanceMileage,
+            PartBrand = dto.PartBrand,
+            PartCost = dto.PartCost,
+            LaborCost = dto.LaborCost,
+        };
+        _context.Services.Add(service);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetById), new { id = service.Id }, service);
     }
 }
