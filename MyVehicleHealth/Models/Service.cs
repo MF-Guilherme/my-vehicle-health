8d538689498185a9de04c0dc7bc4933398f3f659
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2025-01-24 16:29:57 -0300

    Adding an empty constructor to Service

--- MyVehicleHealth/Models/Service.cs
+++ MyVehicleHealth/Models/Service.cs
@@ -12,8 +12,10 @@ public class Service
     public string? PartBrand { get; set; }
     public decimal PartCost { get; set; }
     public decimal LaborCost { get; set; }
-    
+
     public Maintenance Maintenance { get; set; }
+
+    public Service() { }
 
     public Service(int maintenanceId, string description, DateTime maintenanceDate, DateTime nextMaintenanceDate, decimal partCost, decimal laborCost)
     {
