b8ab6c59059df59bf25ce0da02890fa4340cf8c2
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2025-03-31 10:17:48 -0300

    feat(cqrs-refactor): GetAllServicesQuery.cs implemented.

--- backend/MyVehicleHealth/API/Controllers/ServiceController.cs
+++ backend/MyVehicleHealth/API/Controllers/ServiceController.cs
@@ -1,6 +1,8 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyVehicleHealth.Application.Service.Dtos;
+using MyVehicleHealth.Application.Service.Queries;
 using MyVehicleHealth.Domain.Entities;
 using MyVehicleHealth.Infrastructure.Data;
 
@@ -10,153 +12,117 @@ namespace MyVehicleHealth.API.Controllers;
 [Route("api/[controller]")]
 public class ServiceController : ControllerBase
 {
-    private readonly AppDbContext _context;
+    private readonly IMediator _mediator;
 
-    public ServiceController(AppDbContext context)
+    public ServiceController(IMediator mediator)
     {
-        _context = context;
+        _mediator = mediator;
     }
 
     [HttpGet]
-    public IActionResult GetAll([FromQuery] ServiceFilterDto filter)
+    public async Task<IActionResult> GetAll([FromQuery] ServiceFilterDto filter)
     {
-        var query = _context.Services
-            .Include(s => s.Maintenance)
-            .AsQueryable(); // Permite adicionar filtros dinamicamente
-
-        // Filtro por descrição (LIKE)
-        if (!string.IsNullOrEmpty(filter.Description))
-        {
-            query = query.Where(s => s.Description.Contains(filter.Description));
-        }
-        
-        // Filtro por data de manutenção
-        if (filter.MaintenanceDate.HasValue)
-        {
-            query = query.Where(s => s.MaintenanceDate.Date == filter.MaintenanceDate.Value.Date);
-        }
-        
-        // Filtro por data da próxima manutenção
-        if (filter.NextMaintenanceDate.HasValue)
-        {
-            query = query.Where(s => s.NextMaintenanceDate.Date == filter.NextMaintenanceDate.Value.Date);
-        }
-        
-        var services = query
-            .Select(s => new ServiceReadDto
-            {
-                Id = s.Id,
-                MaintenanceId = s.MaintenanceId,
-                Description = s.Description,
-                PartCost = s.PartCost,
-                LaborCost = s.LaborCost,
-                MaintenanceDate = s.MaintenanceDate,
-                NextMaintenanceDate = s.NextMaintenanceDate,
-                CurrentMileage = s.CurrentMileage,
-                NextMaintenanceMileage = s.NextMaintenanceMileage,
-                PartBrand = s.PartBrand
-            })
-            .ToList();
+        var services = await _mediator.Send(new GetAllServicesQuery(filter));
         return Ok(services);
 
     }
-    
-    [HttpGet("{id}")]
-    public IActionResult GetById(int id)
-    {
-        var service = _context.Services
-            .Where(s => s.Id == id)
-            .Include(s => s.Maintenance)
-            .Select(s => new ServiceReadDto
-            {
-                Id = s.Id,
-                MaintenanceId = s.MaintenanceId,
-                Description = s.Description,
-                PartCost = s.PartCost,
-                LaborCost = s.LaborCost,
-                MaintenanceDate = s.MaintenanceDate,
-                NextMaintenanceDate = s.NextMaintenanceDate,
-                CurrentMileage = s.CurrentMileage,
-                NextMaintenanceMileage = s.NextMaintenanceMileage,
-                PartBrand = s.PartBrand
-            })
-            .FirstOrDefault();
-        return Ok(service);
-
-    }
-    
-    [HttpGet("maintenance/{maintenanceId}")]
-    public IActionResult GetAllServicesForMaintenance(int maintenanceId)
-    {
-        var services = _context.Services
-            .Where(s => s.MaintenanceId == maintenanceId)
-            .Include(s => s.Maintenance)
-            .Select(s => new ServiceReadDto
-            {
-                Id = s.Id,
-                MaintenanceId = s.MaintenanceId,
-                Description = s.Description,
-                PartCost = s.PartCost,
-                LaborCost = s.LaborCost,
-                MaintenanceDate = s.MaintenanceDate,
-                NextMaintenanceDate = s.NextMaintenanceDate,
-                CurrentMileage = s.CurrentMileage,
-                NextMaintenanceMileage = s.NextMaintenanceMileage,
-                PartBrand = s.PartBrand
-            })
-            .ToList();
-        return Ok(services);
-
-    }
-    
-    [HttpPost]
-    public IActionResult Create(int maintenanceId, ServiceCreateDto dto)
-    {
-        var service = new Service
-        {
-            MaintenanceId = maintenanceId,
-            Description = dto.Description,
-            MaintenanceDate = dto.MaintenanceDate,
-            CurrentMileage = dto.CurrentMileage,
-            NextMaintenanceDate = dto.NextMaintenanceDate,
-            NextMaintenanceMileage = dto.NextMaintenanceMileage,
-            PartBrand = dto.PartBrand,
-            PartCost = dto.PartCost,
-            LaborCost = dto.LaborCost,
-        };
-        _context.Services.Add(service);
-        _context.SaveChanges();
-        return CreatedAtAction(nameof(GetById), new { id = service.Id }, service);
-    }
-
-    [HttpPut("{id}")]
-    public IActionResult Update(int id, ServiceUpdateDto dto)
-    {
-        var service = _context.Services.Find(id);
-        if (service is null) return NotFound();
-        
-        service.Description = dto.Description;
-        service.MaintenanceDate = dto.MaintenanceDate;
-        service.CurrentMileage = dto.CurrentMileage;
-        service.NextMaintenanceDate = dto.NextMaintenanceDate;
-        service.NextMaintenanceMileage = dto.NextMaintenanceMileage;
-        service.PartBrand = dto.PartBrand;
-        service.PartCost = dto.PartCost;
-        service.LaborCost = dto.LaborCost;
-        
-        _context.SaveChanges();
-        return CreatedAtAction(nameof(GetById), new { id = service.Id }, service);
-    }
-
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-    {
-        var service = _context.Services.Find(id);
-        if (service is null) return NotFound();
-        _context.Services.Remove(service);
-        _context.SaveChanges();
-        return NoContent();
-    }
+    //
+    // [HttpGet("{id}")]
+    // public IActionResult GetById(int id)
+    // {
+    //     var service = _context.Services
+    //         .Where(s => s.Id == id)
+    //         .Include(s => s.Maintenance)
+    //         .Select(s => new ServiceReadDto
+    //         {
+    //             Id = s.Id,
+    //             MaintenanceId = s.MaintenanceId,
+    //             Description = s.Description,
+    //             PartCost = s.PartCost,
+    //             LaborCost = s.LaborCost,
+    //             MaintenanceDate = s.MaintenanceDate,
+    //             NextMaintenanceDate = s.NextMaintenanceDate,
+    //             CurrentMileage = s.CurrentMileage,
+    //             NextMaintenanceMileage = s.NextMaintenanceMileage,
+    //             PartBrand = s.PartBrand
+    //         })
+    //         .FirstOrDefault();
+    //     return Ok(service);
+    //
+    // }
+    //
+    // [HttpGet("maintenance/{maintenanceId}")]
+    // public IActionResult GetAllServicesForMaintenance(int maintenanceId)
+    // {
+    //     var services = _context.Services
+    //         .Where(s => s.MaintenanceId == maintenanceId)
+    //         .Include(s => s.Maintenance)
+    //         .Select(s => new ServiceReadDto
+    //         {
+    //             Id = s.Id,
+    //             MaintenanceId = s.MaintenanceId,
+    //             Description = s.Description,
+    //             PartCost = s.PartCost,
+    //             LaborCost = s.LaborCost,
+    //             MaintenanceDate = s.MaintenanceDate,
+    //             NextMaintenanceDate = s.NextMaintenanceDate,
+    //             CurrentMileage = s.CurrentMileage,
+    //             NextMaintenanceMileage = s.NextMaintenanceMileage,
+    //             PartBrand = s.PartBrand
+    //         })
+    //         .ToList();
+    //     return Ok(services);
+    //
+    // }
+    //
+    // [HttpPost]
+    // public IActionResult Create(int maintenanceId, ServiceCreateDto dto)
+    // {
+    //     var service = new Service
+    //     {
+    //         MaintenanceId = maintenanceId,
+    //         Description = dto.Description,
+    //         MaintenanceDate = dto.MaintenanceDate,
+    //         CurrentMileage = dto.CurrentMileage,
+    //         NextMaintenanceDate = dto.NextMaintenanceDate,
+    //         NextMaintenanceMileage = dto.NextMaintenanceMileage,
+    //         PartBrand = dto.PartBrand,
+    //         PartCost = dto.PartCost,
+    //         LaborCost = dto.LaborCost,
+    //     };
+    //     _context.Services.Add(service);
+    //     _context.SaveChanges();
+    //     return CreatedAtAction(nameof(GetById), new { id = service.Id }, service);
+    // }
+    //
+    // [HttpPut("{id}")]
+    // public IActionResult Update(int id, ServiceUpdateDto dto)
+    // {
+    //     var service = _context.Services.Find(id);
+    //     if (service is null) return NotFound();
+    //     
+    //     service.Description = dto.Description;
+    //     service.MaintenanceDate = dto.MaintenanceDate;
+    //     service.CurrentMileage = dto.CurrentMileage;
+    //     service.NextMaintenanceDate = dto.NextMaintenanceDate;
+    //     service.NextMaintenanceMileage = dto.NextMaintenanceMileage;
+    //     service.PartBrand = dto.PartBrand;
+    //     service.PartCost = dto.PartCost;
+    //     service.LaborCost = dto.LaborCost;
+    //     
+    //     _context.SaveChanges();
+    //     return CreatedAtAction(nameof(GetById), new { id = service.Id }, service);
+    // }
+    //
+    // [HttpDelete("{id}")]
+    // public IActionResult Delete(int id)
+    // {
+    //     var service = _context.Services.Find(id);
+    //     if (service is null) return NotFound();
+    //     _context.Services.Remove(service);
+    //     _context.SaveChanges();
+    //     return NoContent();
+    // }
     
     
 }
---
+++ backend/MyVehicleHealth/Application/Service/Queries/GetAllServicesQuery.cs
@@ -0,0 +1,61 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MyVehicleHealth.Application.Service.Dtos;
+using MyVehicleHealth.Infrastructure.Data;
+
+namespace MyVehicleHealth.Application.Service.Queries;
+
+// Query (recebe os filtros via DTO)
+public record GetAllServicesQuery(ServiceFilterDto Filter) : IRequest<List<ServiceReadDto>>;
+
+// Handler
+public class GetAllServicesQueryHandler : IRequestHandler<GetAllServicesQuery, List<ServiceReadDto>>
+{
+    private readonly AppDbContext _context;
+
+    private GetAllServicesQueryHandler(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<ServiceReadDto>> Handle(GetAllServicesQuery request, CancellationToken cancellationToken)
+    {
+        var query = _context.Services
+            .Include(s => s.Maintenance)
+            .AsQueryable(); // Permite adicionar filtros dinamicamente
+        
+        // Filtro por descrição (LIKE)
+        if (!string.IsNullOrEmpty(request.Filter.Description))
+        {
+            query = query.Where(s => s.Description.Contains(request.Filter.Description));
+        }
+        
+        // Filtro por data de manutenção
+        if (request.Filter.MaintenanceDate.HasValue)
+        {
+            query = query.Where(s => s.MaintenanceDate.Date == request.Filter.MaintenanceDate.Value.Date);
+        }
+        
+        // Filtro por data da próxima manutenção
+        if (request.Filter.NextMaintenanceDate.HasValue)
+        {
+            query = query.Where(s => s.NextMaintenanceDate.Date == request.Filter.NextMaintenanceDate.Value.Date);
+        }
+        
+        return await query
+            .Select(s => new ServiceReadDto
+            {
+                Id = s.Id,
+                MaintenanceId = s.MaintenanceId,
+                Description = s.Description,
+                PartCost = s.PartCost,
+                LaborCost = s.LaborCost,
+                MaintenanceDate = s.MaintenanceDate,
+                NextMaintenanceDate = s.NextMaintenanceDate,
+                CurrentMileage = s.CurrentMileage,
+                NextMaintenanceMileage = s.NextMaintenanceMileage,
+                PartBrand = s.PartBrand
+            })
+            .ToListAsync(cancellationToken);
+    }
+}
